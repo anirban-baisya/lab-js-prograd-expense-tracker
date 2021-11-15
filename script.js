29b4823a429567cefe078c00ff690b1390d629db
anirban-baisya <anirban.baisya2020@vitbhopal.ac.in>
2021-11-15 10:03:50 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let balance = 0.00;
+let income = 0.00;
+let expense =0.00;
+function addStorage()
+{
+    var text=document.getElementById("text").value;
+    var amount= parseFloat(document.getElementById("amount").value);
+    localStorage.setItem(text,amount);
 
+    //document.getElementById('list').innerHTML+=text+"........."+amount + '<br>';
+    
+    if(amount>= 0){
+        income+= amount;
+        document.getElementById('money-plus').innerHTML="+$"+income;
+
+        document.getElementById("list").innerHTML+=`<li class="plus">${text}_______________${amount}</li>`; //retriving local stroag data
+    }
+    else{
+        expense+= amount;
+        document.getElementById('money-minus').innerHTML="$"+expense;
+
+        document.getElementById("list").innerHTML+=`<li class="minus">${text}_______________${amount}</li>`;
+
+    }
+
+    balance= income+expense;
+    document.getElementById('balance').innerHTML="$"+balance;
+
+
+
+
+
+}
